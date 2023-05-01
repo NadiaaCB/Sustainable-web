9350873111f576ac8b471e2e4037b3d9351608a3
NadiaaCB <nadiacb@outlook.dk>
2023-05-01 11:55:23 +0200

    tilføj filer

---
+++ sustainable.js
@@ -0,0 +1,10 @@
+let myName = 123;
+
+console.log(myName);
+
+function changeBackgroundColor() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+}
+
